32362e3809a43c969dd223e023f43a272712ff7e
ydalton <yussef.dalton@gmail.com>
2023-11-03 20:05:24 +0100

    Move connection handling code into new function

--- defs.h
+++ defs.h
@@ -1,15 +1,8 @@
 #ifndef DEFS_H_
 #define DEFS_H_
 
-#include <sys/socket.h>
-
 #define stdout 1
 #define stderr 2
-
-/* syscalls */
-extern unsigned int write(int fd, const char *s, unsigned int count);
-extern void exit(int code);
-extern int close(int fd);
 
 /* functions */
 extern unsigned int strlen(const char *s);
--- main.c
+++ main.c
@@ -1,5 +1,9 @@
+#include <stddef.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #include <sys/socket.h>
+#include <sys/mman.h>
 
 #include "defs.h"
 
@@ -22,15 +26,14 @@ unsigned int eputs(const char *s)
 {
 	return fputs(stderr, s);
 }
-	
-void _start(void)
-{
-	struct sockaddr_in sockaddr;
-	struct sockaddr_in cliaddr;
-	socklen_t addrlen;
-	int sockfd, clientfd;
-	int ret;
 
+int handle_connection(int sockfd)
+{
+	struct sockaddr_in cliaddr;
+	socklen_t addrlen, cliaddrlen;
+	int clientfd, ret;
+	char *mem;
+	size_t recvlen;
 	const char *response =
 		"HTTP/1.0 200 OK\r\n"
 		"Content-Type: text/html\r\n"
@@ -44,6 +47,37 @@ void _start(void)
 		"<p>Hi</p>"
 		"</body>"
 		"</html>\r\n";
+
+	clientfd = accept(sockfd, (struct sockaddr *) &cliaddr, &addrlen);
+	if(clientfd == -1) {
+		eputs("Failed to accept on socket!\n");
+		return -1;
+	}
+	puts("Connection established!\n");
+
+	mem = mmap(NULL, 4096, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+	recvlen = recvfrom(clientfd, mem, 4096, 0, (struct sockaddr *) &cliaddr, &cliaddrlen);
+	if(recvlen == -1)
+		return -1;
+
+	write(stdout, mem, ret);
+
+	ret = sendto(clientfd, response, strlen(response), 0,
+			(struct sockaddr *) &cliaddr, sizeof(cliaddr));
+	if((unsigned int) ret != strlen(response))
+		eputs("Warning: didn't send all bytes");
+	munmap(mem, 4096);
+	shutdown(clientfd, SHUT_RDWR);
+	close(clientfd);
+	return 0;
+}
+	
+void _start(void)
+{
+	struct sockaddr_in sockaddr;
+	int sockfd;
+	int ret;
 
 	puts("Starting tinyhttpd HTTP server...\n");
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -69,18 +103,11 @@ void _start(void)
 	}
 
 	while(1) {
-		clientfd = accept(sockfd, (struct sockaddr *) &cliaddr, &addrlen);
-		if(clientfd == -1) {
-			eputs("Failed to accept on socket!\n");
+		ret = handle_connection(sockfd);
+		if(ret != 0) {
+			eputs("Failed to handle connection!\n");
 			goto err;
 		}
-		puts("Connection established!\n");
-		ret = sendto(clientfd, response, strlen(response), 0,
-				(struct sockaddr *) &cliaddr, sizeof(cliaddr));
-		if((unsigned int) ret != strlen(response))
-			eputs("Warning: didn't send all bytes");
-		shutdown(clientfd, SHUT_RDWR);
-		close(clientfd);
 	}
 	close(sockfd);
 	exit(0);
